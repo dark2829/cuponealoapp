2b4af53d568a8ce848cd252ee1da672b4e78e9f4
Alejandro <tozcuento.2829@gmail.com>
2023-03-14 11:02:37 -0600

    botones, iconos, texto

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableNativeFeedback onPress={() => Alert.alert("Tocaste el texto")}>
+        <Text>Hola mundo</Text>
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
     </View>
   );
